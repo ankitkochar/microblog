b2b4fb497c157134c740595fd22f64d56f633686
ankit <ankitkochar456@gmail.com>
2021-06-04 04:30:44 +0530

    config file 2 for mail serving

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = os.environ.get('MAIL_SERVER') or "smtp.googlemail.com"
 	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
-	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
+	# MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
+    MAIL_USE_SSL = True
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "ankitkochar456@gmail.com"
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "ankit123"
 	ADMINS = ['ankitkochar456@gmail.com']
