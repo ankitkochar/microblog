8b409e46e990b70597e3e2d247c54e62daebc863
ankit <ankitkochar456@gmail.com>
2021-06-04 04:13:42 +0530

    mail server

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = os.environ.get('MAIL_SERVER') or "smtp.googlemail.com"
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "ankitkochar456@gmail.com"
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "ankit123"
