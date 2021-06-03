cb504080082520782cd5fb7247fdce1be580da2a
ankit <ankitkochar456@gmail.com>
2021-06-04 04:44:10 +0530

    changes in init and email support

--- app/__init__.py
+++ app/__init__.py
@@ -23,8 +23,8 @@ if not app.debug:
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
+        # if app.config['MAIL_USE_TLS']:
+        secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
