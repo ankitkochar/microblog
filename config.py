b89bdceab32efa1877a0639a0650abdfa76d2131
ankit <ankitkochar456@gmail.com>
2021-06-04 04:32:36 +0530

    config file 3 for mail serving

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
+	# ...
 	SECRET_KEY=os.environ.get("SECRET_KEY") or "ankit"
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = os.environ.get('MAIL_SERVER') or "smtp.googlemail.com"
 	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
 	# MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
-    MAIL_USE_SSL = True
+	MAIL_USE_SSL = True
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "ankitkochar456@gmail.com"
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "ankit123"
 	ADMINS = ['ankitkochar456@gmail.com']
